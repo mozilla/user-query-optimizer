74923620dc46e0c6d5ae05020451c60c16f9d088
Swathi Iyer <swathii@stanford.edu>
2018-07-17 14:13:08 -0700

    base optimization check workflow with initial checks

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import sys
 import re
 
-def parseQuery(query):
-    keywords = ["select", "where", "from", "group by", "order by"]
+# Suggest using approximate algorithms (e.g. approx_distinct()
+# instead of COUNT(DISTINCT ...))
+def checkApproximates(query):
+    pass
+
+# Suggest selecting the columns the user wants explicitly,
+# rather than using (SELECT *)
+def checkColumnSelection(query):
+    if re.search('select \*', query) is not None:
+        print('\tIf you do not need all columns of the table, select the columns you want explicitly.\n')
+
+# Suggest filtering on partitioned columns
+def checkPartitions(query):
+    pass
+
+# Execute each optimization check
+def giveHints(query):
+    checkApproximates(query)
+    checkColumnSelection(query)
+    checkPartitions(query)
 
 
 if __name__ == '__main__':
@@ -15,4 +33,10 @@ if __name__ == '__main__':
     query = query.lower()
 
     # Break into subqueries
-    subqueries = ["select" + q for q in query.split("select")]
+    subqueries = ['select' + q for q in query.split('select')]
+
+    for query in subqueries:
+        print(query)
+        print("Optimizations:")
+        giveHints(query)
+        print("\n")
