1955ac76e573323bf4cd5ad3e296b888e0e8520d
Swathi Iyer <swathii@stanford.edu>
2018-07-18 15:54:40 -0700

    optimizer api stub

---
+++ optimizer.py
@@ -0,0 +1,9 @@
+import re
+
+class Optimizer:
+    def __init__(self):
+        raise NotImplemented
+
+    # Input: query, schema; Output: Optimization recommendations
+    def optimize_query(query, schema):
+        raise NotImplemented
