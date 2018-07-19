e00ec378a15fb1af4d3322a5faf7d88eefc28159
Swathi Iyer <swathii@stanford.edu>
2018-07-19 16:26:19 -0700

    query formatting, refactoring

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
 import sys
-import re
-
-# Suggest using approximate algorithms (e.g. approx_distinct()
-# instead of COUNT(DISTINCT ...))
-def checkApproximates(query):
-    pass
-
-# Suggest selecting the columns the user wants explicitly,
-# rather than using (SELECT *)
-def checkColumnSelection(query):
-    if re.search('select \*', query) is not None:
-        print('\tIf you do not need all columns of the table, select the columns you want explicitly.\n')
-
-# Suggest filtering on partitioned columns
-def checkPartitions(query):
-    pass
-
-# Execute each optimization check
-def giveHints(query):
-    checkApproximates(query)
-    checkColumnSelection(query)
-    checkPartitions(query)
-
+import optimizer
 
 if __name__ == '__main__':
     # Read File
@@ -31,12 +9,10 @@ if __name__ == '__main__':
 
     # Ignore case
     query = query.lower()
+    schema = {} # placeholder
 
-    # Break into subqueries
-    subqueries = ['select' + q for q in query.split('select')]
+    # Instantiate optimizer object
+    optimizer = optimizer.Optimizer(query, schema)
 
-    for query in subqueries:
-        print(query)
-        print("Optimizations:")
-        giveHints(query)
-        print("\n")
+    # Optimize query   
+    optimizer.optimize_query()
--- optimizer.py
+++ optimizer.py
@@ -1,9 +1,66 @@
+from collections import defaultdict
 import re
+import sqlparse
 
 class Optimizer:
-    def __init__(self):
-        raise NotImplemented
+    def __init__(self, query, schema):
+        # This probably doesn't need to be stored like this? Will keep for now
+        self.query = query
+        self.schema = schema
+        self.optimizationRef = dict([
+            (1, "Check Approximates"),
+            (2, "Check Column Selection"),
+            (3, "Check Partitions")
+        ])
 
     # Input: query, schema; Output: optimization recommendations
-    def optimize_query(query, schema):
-        raise NotImplemented
+    def optimize_query(self):
+        # Format Query
+        formatted_query = sqlparse.format(self.query, reindent = True, keyword_case = 'upper')
+
+        # Switch from unicode to utf-8 encoding
+        formatted_query = formatted_query.encode('utf-8')
+
+        # Split so optimizations can be made with reference to specific lines in query
+        lines = formatted_query.splitlines()
+
+        # Run all optimization checks
+        optimizations = self.__runOptimizationChecks(lines)
+
+        # Print lines with line numbers
+        for ind, l in enumerate(lines):
+            print(str(ind + 1) + " " + l)
+
+        # Print optimizations for each line
+        print(optimizations)
+
+
+    def __runOptimizationChecks(self, lines):
+        # Dictionary: line -> list of optimizations
+        optimizations = defaultdict(list)
+
+        # Run tests
+        self.__checkApproximates(lines, optimizations)
+        self.__checkColumnSelection(lines, optimizations)
+        self.__checkPartitions(lines, optimizations)
+
+        return optimizations
+
+    # Optimization #1
+    #   Suggest using approximate algorithms (e.g. approx_distinct()
+    #   instead of COUNT(DISTINCT ...))
+    def __checkApproximates(self, lines, optimizations):
+        pass
+
+    # Optimization # 2
+    #   Suggest selecting the columns the user wants explicitly,
+    #   rather than using (SELECT *)
+    def __checkColumnSelection(self, lines, optimizations):
+        for ind, l in enumerate(lines):
+            if re.search("SELECT \*", l) is not None:
+                optimizations[ind] += [2]
+
+    # Optimization # 3
+    #    Suggest filtering on partitioned columns
+    def __checkPartitions(self, lines, optimizations):
+        pass
