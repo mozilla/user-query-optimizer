05b4c5761743140b7ed8b684617b6e0625f44e91
Swathi Iyer <swathii@stanford.edu>
2018-08-08 15:36:56 -0700

    new optimizer for each db, new file for approx optimization

--- tests/test_opts.py
+++ tests/test_opts.py
@@ -6,38 +6,38 @@ sys.path.append('../user-query-optimizer')
 import optimizer
 import sqlparse
 
-def test_approximates(queries, op):
-    print("\n")
-    # dictionary from test-query-file -> list of line numbers in that query with an approx optimization
-    correct_opts = {
-        'test-query-3.txt': [0]
-    }
-    test_ops = {}
-
-    for ind, query in enumerate(queries):
-        # Parse query and extract ctes
-        # Strip comments to help sqlparse correctly extract the identifier list
-        formatted_query = str(sqlparse.format(query, strip_comments = True)).strip()
-        parsed_queries = op._parse_query(formatted_query)
-
-        op._checkApproximates(parsed_queries)
-
-        # Find subquery in original query again, and adjust line numbers
-        adjusted_opts = op._adjust_linenums(formatted_query)
-
-        # Print query with line numbers
-        # op._print_query_lines(formatted_query)
-
-        # Print Optimizations
-        # op._print_optimizations(adjusted_opts)
-
-        # Add optimizations for current query to dictionary for all test files
-        if len(adjusted_opts) > 0:
-            test_ops['test-query-' + str(ind + 1) + '.txt'] = adjusted_opts.keys()
-
-
-    assert len(test_ops) == 1
-    assert test_ops == correct_opts
+# def test_approximates(queries, op):
+#     print("\n")
+#     # dictionary from test-query-file -> list of line numbers in that query with an approx optimization
+#     correct_opts = {
+#         'test-query-3.txt': [0]
+#     }
+#     test_ops = {}
+#
+#     for ind, query in enumerate(queries):
+#         # Parse query and extract ctes
+#         # Strip comments to help sqlparse correctly extract the identifier list
+#         formatted_query = str(sqlparse.format(query, strip_comments = True)).strip()
+#         parsed_queries = op._parse_query(formatted_query)
+#
+#         op._checkApproximates(parsed_queries)
+#
+#         # Find subquery in original query again, and adjust line numbers
+#         adjusted_opts = op._adjust_linenums(formatted_query)
+#
+#         # Print query with line numbers
+#         # op._print_query_lines(formatted_query)
+#
+#         # Print Optimizations
+#         # op._print_optimizations(adjusted_opts)
+#
+#         # Add optimizations for current query to dictionary for all test files
+#         if len(adjusted_opts) > 0:
+#             test_ops['test-query-' + str(ind + 1) + '.txt'] = adjusted_opts.keys()
+#
+#
+#     assert len(test_ops) == 1
+#     assert test_ops == correct_opts
 
 def test_column_selection(queries, op):
     print("\n")
@@ -128,7 +128,7 @@ def test_nested_subqueries(queries, op):
         'test-query-10.txt': [4]
     }
     test_ops = {}
-    
+
     for ind, query in enumerate(queries):
         # Parse query and extract ctes
         # Strip comments to help sqlparse correctly extract the identifier list
@@ -153,3 +153,13 @@ def test_nested_subqueries(queries, op):
 
     assert len(test_ops) == 3
     assert test_ops == correct_opts
+
+def test_all(queries, op):
+    test_ops = {}
+
+    for ind, query in enumerate(queries):
+        adjusted_opts = op.optimize_query(query)
+
+        # Add optimizations for current query to dictionary for all test files
+        if len(adjusted_opts) > 0:
+            test_ops['test-query-' + str(ind + 1) + '.txt'] = adjusted_opts.keys()
