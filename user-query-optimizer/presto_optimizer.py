6355e2d9eff3a02d9d9e1625451a502fc81d0a9d
Swathi Iyer <swathii@stanford.edu>
2018-08-08 15:49:01 -0700

    new file for partitions optimization

--- user-query-optimizer/presto_optimizer.py
+++ user-query-optimizer/presto_optimizer.py
@@ -2,13 +2,18 @@ import sys
 sys.path.append('../user-query-optimizer/optimizations')
 import approximates
 import column_selection
+import partitions
 
 class PrestoOptimizer:
     def __init__(self, optimizations, schema):
         self.optimizations = optimizations
+        self.schema = schema
 
     def _checkApproximates(self, parsed_queries):
         approximates.checkApproximates(self.optimizations, parsed_queries)
 
     def _checkColumnSelection(self, parsed_queries):
         column_selection.checkColumnSelection(self.optimizations, parsed_queries)
+
+    def _checkPartitions(self, parsed_queries):
+        partitions.checkPartitions(self.optimizations, self.schema, parsed_queries)
