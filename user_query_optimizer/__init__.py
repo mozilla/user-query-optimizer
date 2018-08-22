2e5c7bbc87ffba51df732884576fff2b2cba582d
Swathi Iyer <swathii@stanford.edu>
2018-08-22 15:15:37 -0700

    pypi package released

--- user_query_optimizer/__init__.py
+++ user_query_optimizer/__init__.py
@@ -11,16 +11,3 @@ def get_optimizer(db, schema):
         return SparkOptimizer(schema)
     else:
         raise ValueError("Database must be one of: 'presto', 'athena', 'spark'")
-
-__version__ = "1.0.0.dev0"
-
-__title__ = "user-query-optimizer"
-__description__ = "SQL query optimization hints"
-__url__ = "https://github.com/mozilla/user-query-optimizer"
-__uri__ = __url__
-__doc__ = __description__ + " <" + __uri__ + ">"
-
-__author__ = "Swathi Iyer"
-__email__ = "swathii@stanford.edu"
-
-__license__ = "MPL"
