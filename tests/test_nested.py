91313ba1d96b6fd9dea0c980d102b7290393c16f
Swathi Iyer <swathii@stanford.edu>
2018-08-31 13:09:19 -0700

    flake8 fixes test_*.py

--- tests/test_nested.py
+++ tests/test_nested.py
@@ -1,10 +1,5 @@
-import re
-from collections import defaultdict
-from collections import OrderedDict
-import sys
-sys.path.append('../user_query_optimizer')
-import optimizer
 import sqlparse
+
 
 def test_nested_subqueries(queries, presto_op):
     # dictionary from test-query-file -> list of line numbers in that query with an approx optimization
@@ -18,7 +13,7 @@ def test_nested_subqueries(queries, presto_op):
     for ind, query in enumerate(queries):
         # Parse query and extract ctes
         # Strip comments to help sqlparse correctly extract the identifier list
-        formatted_query = str(sqlparse.format(query, strip_comments = True)).strip()
+        formatted_query = str(sqlparse.format(query, strip_comments=True)).strip()
         parsed_queries = presto_op._parse_query(formatted_query)
 
         presto_op._extractNestedSubqueries(parsed_queries)
@@ -29,7 +24,6 @@ def test_nested_subqueries(queries, presto_op):
         # Add optimizations for current query to dictionary for all test files
         if len(adjusted_opts) > 0:
             test_ops['test-query-' + str(ind + 1) + '.txt'] = adjusted_opts.keys()
-
 
     assert len(test_ops) == 3
     assert test_ops == correct_ops
